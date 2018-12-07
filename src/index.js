09c5afa37cbac5cdcaa77af8167df35ec40450b7
Hannah Kang <kyouyatamax@gmail.com>
2018-12-06 20:49:29 -0800

    initial commit, setting up github pages hosting

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class DataBaseShell extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>This is where the header will go</h1> 
+        <h2>This is where the database will go</h2> 
+        <h3>And this will be the credits/footer information</h3>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<DataBaseShell />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
