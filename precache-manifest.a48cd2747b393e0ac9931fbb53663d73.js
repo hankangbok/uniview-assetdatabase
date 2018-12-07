cdda18fa2b48a382f73f6f58f483bc3c048b020b
Hannah Kang <kyouyatamax@gmail.com>
2018-12-06 20:53:55 -0800

    Updates

---
+++ precache-manifest.a48cd2747b393e0ac9931fbb53663d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uniview-assetdatabase/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d588570a4e3f26efde88",
+    "url": "/uniview-assetdatabase/static/js/runtime~main.d588570a.js"
+  },
+  {
+    "revision": "43052787b29484ba7a5f",
+    "url": "/uniview-assetdatabase/static/js/main.43052787.chunk.js"
+  },
+  {
+    "revision": "5b265051865492349dc6",
+    "url": "/uniview-assetdatabase/static/js/1.5b265051.chunk.js"
+  },
+  {
+    "revision": "43052787b29484ba7a5f",
+    "url": "/uniview-assetdatabase/static/css/main.bef847d2.chunk.css"
+  },
+  {
+    "revision": "04f94ae1b756c1a36b2622f5347f5ee0",
+    "url": "/uniview-assetdatabase/index.html"
+  }
+];
